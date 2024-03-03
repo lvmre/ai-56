80f66c1e92eda4f8acc83cba053bad1f6c0ed9b1
LVMRE <lovemorec@gmail.com>
2024-03-03 22:33:55 +0200

    D

---
+++ patrol/app/src/main/java/wktechsys/com/guardprotection/Utilities/Constant.java
@@ -0,0 +1,18 @@
+package wktechsys.com.guardprotection.Utilities;
+
+public class Constant {
+
+    public static final String LOGIN_URL = "http://sagarfoundation.org.in/guardadmin/Api/login";
+    public static final String GETDATA_URL = "http://sagarfoundation.org.in/guardadmin/Api/get_guards";
+    public static final String SCAN_URL = "http://sagarfoundation.org.in/guardadmin/Api/scan_checkpoint";
+    public static final String ABOUT_URL = "http://sagarfoundation.org.in/guardadmin/Api/about_us";
+    public static final String ATTENDANCE_URL = "http://sagarfoundation.org.in/guardadmin/Api/get_attendance";
+    public static final String MISSEDPT_URL = "http://sagarfoundation.org.in/guardadmin/Api/get_missed";
+    public static final String LOGOUT_URL = "http://sagarfoundation.org.in/guardadmin/Api/sign_out";
+    public static final String FORGETPASSWORD_URL = "http://sagarfoundation.org.in/guardadmin/Api/forget_password";
+    public static final String ROUND_URL = "http://sagarfoundation.org.in/guardadmin/Api/get_rounds";
+    public static final String HISTORY_URL = "http://sagarfoundation.org.in/guardadmin/Api/r_history";
+    public static final String DETAILED_URL = "http://sagarfoundation.org.in/guardadmin/Api/r_history_detailed";
+    public static final String HISTORYSORT_URL = "http://sagarfoundation.org.in/guardadmin/Api/history_by_date";
+
+}
